4b7797f98dd13f8f223b6bad4baefa7bfc4cb004
stebunoff <mailbox@stebunoff.ru>
2024-02-11 13:58:31 +0300

    feat: add user container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -2,10 +2,14 @@ import 'reflect-metadata';
 import { RestApplication, createRestApplicationContainer } from './rest/index.js';
 import { Container } from 'inversify';
 import { Component } from './shared/types/component.js';
+import { createUserContainer } from './shared/modules/user/user.container.js';
 
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+    );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
