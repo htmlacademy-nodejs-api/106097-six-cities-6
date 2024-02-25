dcddaefb29eb051bf5b0d4250f3c8720be9d577b
stebunoff <mailbox@stebunoff.ru>
2024-02-25 13:37:33 +0300

    feat: extend offer service interface

---
+++ src/shared/modules/offer/dto/update-offer.dto.ts
@@ -0,0 +1,23 @@
+import { City } from '../../../types/city.js';
+import { User } from '../../../types/user.type.js';
+
+export class updateOfferDto {
+  public name?: string;
+  public description?: string;
+  public postDate?: Date;
+  public town?: City;
+  public previewPath?: string;
+  public photos?: string[];
+  public premium?: boolean;
+  public favorite?: boolean;
+  public rating?: number;
+  public type?: string;
+  public rooms?: number;
+  public guests?: number;
+  public price?: number;
+  public amenities?: string[];
+  public author?: User;
+  public commentCount?: number;
+  public latitude?: number;
+  public longitude?: number;
+}
--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,8 +1,15 @@
 import { DocumentType } from '@typegoose/typegoose';
-import { CreateOfferDto, OfferEntity } from './index.js';
+import { CreateOfferDto, OfferEntity, updateOfferDto } from './index.js';
+import { City } from '../../types/city.js';
 
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: updateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findPremium(city: City): Promise<DocumentType<OfferEntity>[]>
+  addToFavorites(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  removeFromFavorites(offerId: string): Promise<DocumentType<OfferEntity> | null>;
 }
