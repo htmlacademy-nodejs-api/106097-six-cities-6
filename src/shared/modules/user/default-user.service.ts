e73bdacc8defc0f758e4e075f67c393b85cb8fd6
stebunoff <mailbox@stebunoff.ru>
2024-02-26 12:20:03 +0300

    feat: extend user interface

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,10 +1,8 @@
-import { UserService } from './user-service.interface.js';
 import { DocumentType, types } from '@typegoose/typegoose';
-import { UserEntity } from './user.entity.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/component.js';
 import { Logger } from '../../libs/logger/index.js';
+import { UpdateUserDto, CreateUserDto, UserEntity, UserService } from './index.js';
 
 @injectable()
 export class DefaultUserService implements UserService {
@@ -26,8 +24,8 @@ export class DefaultUserService implements UserService {
     return this.userModel.findOne({email});
   }
 
-  public async findById(id: string): Promise<DocumentType<UserEntity> | null> {
-    return this.userModel.findOne({id});
+  public async findById(userId: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({userId});
   }
 
   public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
@@ -38,5 +36,11 @@ export class DefaultUserService implements UserService {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel
+      .findByIdAndUpdate(userId, dto, {new: true})
+      .exec();
   }
 }
